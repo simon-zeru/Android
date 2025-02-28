f1fcedfca4170135cace898f0055a92b40b6d4a5
simon-zeru <sim.zeru@gmail.com>
2025-02-28 09:46:09 +0100

    TP4: Add result handling and navigation
    - Adds handling of user responses in `TableMultiplicationActivity`.
    - Adds navigation to `FelicitationActivity` if all answers are correct, `ErreurActivity` otherwise.
    - Adds display of number of errors in `ErreurActivity`.
    - Adds a button to return to previous screen from `ErreurActivity`.
    - Modifies layouts.

--- AndroidTP/app/src/main/java/fr/iut2/androidtp/ErreurActivity.java
+++ AndroidTP/app/src/main/java/fr/iut2/androidtp/ErreurActivity.java
@@ -1,6 +1,9 @@
 package fr.iut2.androidtp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ErreurActivity extends AppCompatActivity {
 
+    public static final String NB_ERRORS = "nb_errors";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_erreur);
+
+        int nb_errors = getIntent().getIntExtra(NB_ERRORS, 0);
+
+        TextView nb_errors_view = findViewById(R.id.exercice5_text_nb_erreurs);
+        nb_errors_view.setText("Nombre d'erreurs : "+nb_errors);
+
+        Button comeback = findViewById(R.id.exercice5_corriger_reponses);
+
+        comeback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                finish();
+            }
+        });
 
 
     }
--- AndroidTP/app/src/main/java/fr/iut2/androidtp/TableMultiplicationActivity.java
+++ AndroidTP/app/src/main/java/fr/iut2/androidtp/TableMultiplicationActivity.java
@@ -1,6 +1,8 @@
 package fr.iut2.androidtp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,24 +15,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 import fr.iut2.androidtp.exercice3Data.Multiplication;
 import fr.iut2.androidtp.exercice3Data.tableMultiplication;
 
 public class TableMultiplicationActivity extends AppCompatActivity {
 
     public static final String TABLE_KEY = "table_key";
+    private ArrayList<EditText> userResponses = new ArrayList<>();
+    private static final int SCORE_MAX = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_table_multiplication);
+
+
+
+
         int table = getIntent().getIntExtra(TABLE_KEY, 1);
         tableMultiplication tableMult = new tableMultiplication(table, true);
 
         LinearLayout linear = findViewById(R.id.exercice5_table_multiplication);
+
+
+
         for (Multiplication multiplication:
-             tableMult.getMultiplications()) {
+                tableMult.getMultiplications()) {
             // 2. Création de la ligne temporaire
             LinearLayout linearTMP = (LinearLayout) getLayoutInflater().inflate(R.layout.template_calcul, null);
 
@@ -40,8 +54,7 @@ public class TableMultiplicationActivity extends AppCompatActivity {
 
             // 4. Création de l'EditText permettant d'interagir avec l'utilisateur
             EditText resultat = (EditText) linearTMP.findViewById(R.id.template_resultat);
-
-            resultat.setText(Integer.toString(multiplication.getResultat()));
+            userResponses.add(resultat);
 
             // 5. Ajout au Linear principal
             linear.addView(linearTMP);
@@ -54,6 +67,29 @@ public class TableMultiplicationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Il faut compter le nombre de réponses justes
                 // puis rediriger vers l'activité Felicitation ou Erreur
+                int i = 0;
+                for (Multiplication multiplication:
+                        tableMult.getMultiplications()) {
+                    EditText res = userResponses.get(i);
+                    if (res == null) {
+                        multiplication.setReponseUtilisateur(-1);
+                    } else {
+                        Log.d("Reponse ligne", res.getText().toString());
+                        multiplication.setReponseUtilisateur(Integer.parseInt(res.getText().toString()));
+                    }
+                    i++;
+                }
+                int score = tableMult.getNbReponsesJustes();
+                int errors = SCORE_MAX - score;
+                Intent intent;
+                if (errors == 0) {
+                    intent = new Intent(TableMultiplicationActivity.this, FelicitationActivity.class);
+
+                } else {
+                    intent = new Intent(TableMultiplicationActivity.this, ErreurActivity.class);
+                    intent.putExtra(ErreurActivity.NB_ERRORS, errors);
+                }
+                startActivity(intent);
             }
         });
 
